edee047d80473bdf304ffc6f6415c0651b4afca8
David Füchslin <david.fuchslin@gmail.com>
2019-12-28 21:25:57 +0100

    format

--- import_el.py
+++ import_el.py
@@ -8,58 +8,58 @@ import openpyxl
 
 
 def parse_and_import_file(config):
-  workbook = openpyxl.load_workbook(config['data_file'])
-  worksheet = workbook.active
-  for row in worksheet.values:
-    parse_and_import_row(config, row)
+    workbook = openpyxl.load_workbook(config['data_file'])
+    worksheet = workbook.active
+    for row in worksheet.values:
+        parse_and_import_row(config, row)
 
 
 def parse_and_import_row(config, row):
-  try:
-    data = parse_row(config, row)
-  except Exception as e:
-    print("Skipping unparseable row '{}': {}".format(row, e))
-    return
+    try:
+        data = parse_row(config, row)
+    except Exception as e:
+        print("Skipping unparseable row '{}': {}".format(row, e))
+        return
 
-  try:  
-    import_data(config, data)
-  except Exception as e:
-    print("Problem importing row '{}': {}".format(row, e))
+    try:
+        import_data(config, data)
+    except Exception as e:
+        print("Problem importing row '{}': {}".format(row, e))
 
 
 def parse_row(config, row):
-  data = {}
-  data['timestamp'] = parse_timestamp(config, row[0])
-  data['watts'] = row[1] * 1000
-  return data
+    data = {}
+    data['timestamp'] = parse_timestamp(config, row[0])
+    data['watts'] = row[1] * 1000
+    return data
 
 
 def parse_timestamp(config, timestamp):
-  return int(datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M').replace(tzinfo=pytz.timezone(config['data_timezone'])).timestamp())
+    return int(datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M').replace(tzinfo=pytz.timezone(config['data_timezone'])).timestamp())
 
 
 def import_data(config, data):
-  msg = "{} {:0.0f} {:d}\n".format(config['metric_path'], data['watts'], data['timestamp'])
-  print("importing to {}:{}  [{}]".format(config['graphite_host'], config['graphite_port'], msg.replace('\n', '')), flush = True)
-  sock = socket.socket()
-  sock.connect((config['graphite_host'], config['graphite_port']))
-  sock.sendall(msg.encode())
-  sock.close()
+    msg = "{} {:0.0f} {:d}\n".format(config['metric_path'], data['watts'], data['timestamp'])
+    print("importing to {}:{}  [{}]".format(config['graphite_host'], config['graphite_port'], msg.replace('\n', '')), flush=True)
+    sock = socket.socket()
+    sock.connect((config['graphite_host'], config['graphite_port']))
+    sock.sendall(msg.encode())
+    sock.close()
 
 
 def main(argv):
-  if len(argv) < 3:
-    print("python ./import_el.py [excel_file] [graphite_host]")
-    sys.exit(2)
+    if len(argv) < 3:
+        print("python ./import_el.py [excel_file] [graphite_host]")
+        sys.exit(2)
 
-  config = {}
-  config['data_file'] = argv[1]
-  config['graphite_host'] = argv[2]
-  config['graphite_port'] = 2003
-  config['metric_path'] = 'energy.household.total.watthours.count'
-  config['data_timezone'] = 'Europe/Stockholm'
+    config = {}
+    config['data_file'] = argv[1]
+    config['graphite_host'] = argv[2]
+    config['graphite_port'] = 2003
+    config['metric_path'] = 'energy.household.total.watthours.count'
+    config['data_timezone'] = 'Europe/Stockholm'
 
-  parse_and_import_file(config)
+    parse_and_import_file(config)
 
 
 main(sys.argv)
